dd3685f0b6f8d406117592b430d4cdb17edaee2c
user <user@users-iMac.lan>
2020-09-24 15:59:30 +0800

    Leetcode: init intToRoman

---
+++ leetcode/intToRoman/intToRoman.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    string intToRoman(int num)
+    {
+        char* c[4][10] = {
+            {"","I","II","III","IV","V","VI","VII","VIII","IX"},
+            {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"},
+            {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"},
+            {"","M","MM","MMM"}
+        };
+        string roman;
+        roman.append(c[3][num / 1000]);
+        roman.append(c[2][num / 100 % 10]);
+        roman.append(c[1][num / 10 % 10]);
+        roman.append(c[0][num % 10]);
+         
+        return roman;
+    }
+};
+
+int main(int argc, char** argv)
+{
+    Solution sl;
+    cout << sl.intToRoman(1994) << endl;
+}
