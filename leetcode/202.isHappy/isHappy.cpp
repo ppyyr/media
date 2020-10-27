466317dbdce808bd9c28b0c48ea7bfacaf19fd7d
Saul <ppyyr@live.jp>
2020-10-27 10:43:39 +0800

    Leetcode: init 202 isHappy

---
+++ leetcode/202.isHappy/isHappy.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <unordered_set>
+
+using namespace std;
+
+class Solution {
+public:
+    int getSum(int n)
+    {
+        int sum = 0;
+        while(n)
+        {
+            sum += (n % 10) * (n % 10);
+            n /= 10;
+        }
+        return sum;
+    }
+    bool isHappy(int n) {
+        unordered_set<int> set;
+        while(1)
+        {
+            int sum = getSum(n);
+            if(sum == 1)
+                return true;
+            if(set.find(sum) != set.end())
+            {
+                return false;
+            }
+            else
+            {
+                set.insert(sum);
+            }
+            n = sum;
+        }
+    }
+};
+
+int main(int argc, char** argv)
+{
+    Solution sl;
+    bool r = sl.isHappy(19);
+}
