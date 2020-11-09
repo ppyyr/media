c35998ba517526c724c84c6c3b0ecd0fdbc4c676
Saul Lee <saul@Sauls-Mac-mini.local>
2020-11-09 17:41:21 +0800

    Leetcode: update divide

--- leetcode/29.divide/divide.cpp
+++ leetcode/29.divide/divide.cpp
@@ -12,49 +12,36 @@ public:
             if(dividend == INT_MIN)
                 return INT_MAX; 
             else
-                return 0 - dividend;
+                return -dividend;
         }
-        if(dividend == 0)
-            return 0;
-        if(divisor == 1)
-            return dividend;
+        if(dividend == 0) return 0;
+        if(divisor == 1) return dividend;
 
-        //转为负数
+        //可能会越界所以转为负数
         int sign = (dividend > 0) ^ (divisor > 0) ? -1 : 1;
-        dividend = dividend > 0 ? 0 - dividend : dividend;
-        divisor = divisor > 0 ? 0 - divisor : divisor;
-
-        //如果被除数不够除就直接退出
-        if (dividend > divisor)
-            return 0;
-
-        int result = 0;
-        while (dividend < 0)
+        int a = dividend > 0 ? -dividend : dividend;
+        int b = divisor > 0 ? -divisor : divisor;
+        if(a > b) return 0;
+        int res = div(a, b);
+        return sign == -1? -res : res;
+            
+    }
+    int div(int a, int b)
+    {
+        if(a > b) return 0;
+        int count = 1;
+        int tb = b;
+        while (tb - a + tb >= 0 && tb + tb < 0)
         {
-            //如果被除数不够除就直接退出
-            if(dividend > divisor)
-                break;
-
-            //开始计算商
-            int divisor_pow_current = divisor;
-            int count = -1;
-            while (true)
-            {
-                //防止除数翻倍后越界
-                if (INT_MIN - divisor_pow_current > divisor_pow_current) 
-                    break;
-
-                //防止除数翻倍后大于被除数
-                if (divisor_pow_current + divisor_pow_current < dividend) 
-                    break;
-                
-                count += count; 
-                //记得除数翻倍
-                divisor_pow_current += divisor_pow_current;
-            }
-            dividend -= divisor_pow_current;
-            result += count;
+            tb += tb;
+            count += count;
         }
-        return sign == 1 ? 0 - result : result; 
+        return count + div(a - tb, b);
     }
 };
+
+int main(int argc, char** argv)
+{
+    Solution sl;
+    int r = sl.divide(INT_MIN, 2);
+}
